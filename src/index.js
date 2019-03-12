b279c33819256a85d4ac74e543e57efc44102a90
RomanDyba <romansmaga@mailfence.com>
2019-03-12 22:04:44 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack =[],
+      leftDNA = bracketsConfig.map(function(p) {return p[0]}),
+      rightDNA = bracketsConfig.map(function(p) {return p[1]});
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == rightDNA[leftDNA.indexOf(stack[stack.length-1])]) 
+      stack.pop()
+    else stack.push(str[i]);
+  }
+  return (!stack.length);
 }
